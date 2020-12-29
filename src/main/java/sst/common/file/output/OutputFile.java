23db5c3178420cffa5d907bbdb7d314fdf37fd8a
Stéphane Stiennon <stephane.stiennon@gmail.com>
2020-12-29 18:19:20 +0100

    sonar

--- src/main/java/sst/common/file/output/OutputFile.java
+++ src/main/java/sst/common/file/output/OutputFile.java
@@ -1,42 +1,38 @@
 package sst.common.file.output;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class OutputFile implements AutoCloseable {
-	private static final String TABULATION = "\t";
-	private static final String LINE_SEPARATOR = "\n";
+    private static final String TABULATION = "\t";
+    private static final String LINE_SEPARATOR = "\n";
 
-	private BufferedWriter output = null;
+    private BufferedWriter output;
 
-	public OutputFile(File file) throws IOException {
-		output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
-	}
+    public OutputFile(File file) throws IOException {
+        output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8));
+    }
 
-	public OutputFile(String filename) throws IOException {
-		output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "UTF-8"));
-	}
+    public OutputFile(String filename) throws IOException {
+        output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), StandardCharsets.UTF_8));
+    }
 
-	public void print(String line) throws IOException {
-		output.write(line);
-		output.flush();
-	}
+    public void print(String line) throws IOException {
+        output.write(line);
+        output.flush();
+    }
 
-	public void println(int tab, String line) throws IOException {
-		for (int i = 0; i < tab; i++, print(TABULATION))
-			;
-		print(line + LINE_SEPARATOR);
-	}
+    public void println(int tab, String line) throws IOException {
+        for (int i = 0; i < tab; i++, print(TABULATION)) ;
+        print(line + LINE_SEPARATOR);
+    }
 
-	public void println(String line) throws IOException {
-		print(line + LINE_SEPARATOR);
-	}
+    public void println(String line) throws IOException {
+        print(line + LINE_SEPARATOR);
+    }
 
-	@Override
-	public void close() throws IOException {
-		output.close();
-	}
+    @Override
+    public void close() throws IOException {
+        output.close();
+    }
 }
--- src/main/java/sst/common/file/replacefile/ReplaceFile.java
+++ src/main/java/sst/common/file/replacefile/ReplaceFile.java
@@ -1,26 +1,24 @@
-package sst.common.file.replaceFile;
+package sst.common.file.replacefile;
+
+import sst.common.file.output.OutputFile;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 
-import sst.common.file.output.OutputFile;
-
 public class ReplaceFile {
-    private HashMap<String, String> dictionnary = new HashMap<>();
+    private final HashMap<String, String> dictionnary = new HashMap<>();
 
     public void add(String key, String value) {
         dictionnary.put(key, value);
     }
 
-    public void run(String inputFileName, String outputFileName) throws FileNotFoundException, IOException {
-        try (BufferedReader input = new BufferedReader(new FileReader(new File(inputFileName)))) {
+    public void run(String inputFileName, String outputFileName) throws IOException {
+        try (BufferedReader input = new BufferedReader(new FileReader(inputFileName))) {
             try (OutputFile output = new OutputFile(outputFileName)) {
-                String line = null;
+                String line;
                 while (null != (line = input.readLine())) {
                     Collection<String> keys = dictionnary.keySet();
                     for (String key : keys) {
